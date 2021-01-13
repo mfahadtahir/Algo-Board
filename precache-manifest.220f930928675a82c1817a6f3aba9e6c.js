5be86614efb6297395a4a5e5f5dfd02dfc8d64ec
Max <muhammadfahad325302@gmail.com>
2021-01-13 21:34:44 +0500

    Updates

---
+++ precache-manifest.220f930928675a82c1817a6f3aba9e6c.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa48ef47b591123e90dfc1549871821",
+    "url": "/Algo-Board/index.html"
+  },
+  {
+    "revision": "8a86949a52ea86c13c7b",
+    "url": "/Algo-Board/static/css/4.b8237279.chunk.css"
+  },
+  {
+    "revision": "ba14ebef1b00b9bd8216",
+    "url": "/Algo-Board/static/css/5.a03c57e8.chunk.css"
+  },
+  {
+    "revision": "0e6c5ae480c015a228ad",
+    "url": "/Algo-Board/static/css/6.dc4a1833.chunk.css"
+  },
+  {
+    "revision": "8a0783d84a859d22e625",
+    "url": "/Algo-Board/static/js/0.4332b7b4.chunk.js"
+  },
+  {
+    "revision": "2a4334a5369b34af77a5",
+    "url": "/Algo-Board/static/js/1.f7a2b2e1.chunk.js"
+  },
+  {
+    "revision": "d492e35f985a7ed89ac6",
+    "url": "/Algo-Board/static/js/10.66c90815.chunk.js"
+  },
+  {
+    "revision": "020fa2acf752dabe71e6",
+    "url": "/Algo-Board/static/js/11.943a6115.chunk.js"
+  },
+  {
+    "revision": "c61a464cc273266805f2",
+    "url": "/Algo-Board/static/js/12.c8adef41.chunk.js"
+  },
+  {
+    "revision": "9558f33fa8659e055f6a",
+    "url": "/Algo-Board/static/js/13.a7443041.chunk.js"
+  },
+  {
+    "revision": "f33c30e75954eb3d431f",
+    "url": "/Algo-Board/static/js/14.fd83e078.chunk.js"
+  },
+  {
+    "revision": "de2c787c428df5f5fbb2",
+    "url": "/Algo-Board/static/js/15.f8e48650.chunk.js"
+  },
+  {
+    "revision": "708c5d2492be432c5516",
+    "url": "/Algo-Board/static/js/16.de7f6402.chunk.js"
+  },
+  {
+    "revision": "5f7c9d94836d994bc6a3",
+    "url": "/Algo-Board/static/js/17.9fc7695b.chunk.js"
+  },
+  {
+    "revision": "eba2c9dbdcb16a2edb5a",
+    "url": "/Algo-Board/static/js/18.a56a8736.chunk.js"
+  },
+  {
+    "revision": "45c05c5be2a29b93ecae",
+    "url": "/Algo-Board/static/js/19.625f501c.chunk.js"
+  },
+  {
+    "revision": "980f5778bc62b1d0bd41",
+    "url": "/Algo-Board/static/js/20.008a66e2.chunk.js"
+  },
+  {
+    "revision": "da6d7d5c69f2685b5f51",
+    "url": "/Algo-Board/static/js/21.2e2945ea.chunk.js"
+  },
+  {
+    "revision": "3e8ad5d57cdce39f2674",
+    "url": "/Algo-Board/static/js/22.e6e72395.chunk.js"
+  },
+  {
+    "revision": "8a86949a52ea86c13c7b",
+    "url": "/Algo-Board/static/js/4.2ce238c0.chunk.js"
+  },
+  {
+    "revision": "ba14ebef1b00b9bd8216",
+    "url": "/Algo-Board/static/js/5.0611c92c.chunk.js"
+  },
+  {
+    "revision": "0e6c5ae480c015a228ad",
+    "url": "/Algo-Board/static/js/6.3f71df1b.chunk.js"
+  },
+  {
+    "revision": "8ac679942a3d5636c278",
+    "url": "/Algo-Board/static/js/7.3611d637.chunk.js"
+  },
+  {
+    "revision": "50045e312396ca7826db",
+    "url": "/Algo-Board/static/js/8.3bbe5310.chunk.js"
+  },
+  {
+    "revision": "5364de3b4f03b0b99649",
+    "url": "/Algo-Board/static/js/9.48eccc2c.chunk.js"
+  },
+  {
+    "revision": "a3541c227f69d6caf819",
+    "url": "/Algo-Board/static/js/main.28a4f7a6.chunk.js"
+  },
+  {
+    "revision": "19d989353593b4b8ea52",
+    "url": "/Algo-Board/static/js/runtime~main.800e2a41.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Algo-Board/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Algo-Board/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Algo-Board/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Algo-Board/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Algo-Board/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
